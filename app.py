8f01a48cee372aa174fd2fe5022c56146738be32
dewypark <dewy0728@gmail.com>
2026-05-12 16:17:17 +0900

    fix port

--- app.py
+++ app.py
@@ -147,4 +147,6 @@ def download_pdf():
     return send_file(buffer, as_attachment=True, download_name='AuditAI_Report.pdf', mimetype='application/pdf')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
